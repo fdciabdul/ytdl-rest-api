0721b254af351f13b59f9d2d9cdfe7641966db63
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2020-10-13 05:30:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,54 @@
-const ytdl = require('ytdl-core');
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors')
 const app = express();
+app.enable('trust proxy');
+const ytdl = require('ytdl-core');
+const ytpl = require('ytpl');
+const secure = require('ssl-express-www');
+
+app.use(morgan('common'));
+app.use(cors())
+app.use(secure)
+app.use(express.json());
 
 app.get('/', async (req, res) => {
-try {
-const info = await ytdl.getInfo(req.query.id);
+  let playlistregex = /\/playlist\?list=/;
+  let videos = []
+  let url = req.query.url;
+  try {
+    if (playlistregex.test(url)) {
+      ytpl(url)
+        .then(info => info.items)
+        .then(info => {
+          let video
+          for (video of info) {
+            videos.push({
+              title: video.title,
+              id: video.id,
+              duration: video.duration,
+            });
+          }
+          res.json(videos)
+        })
+        .catch(err => { 
+          res.statusMessage = "can't download video(s) data. probably wrong url : "+err
+          res.sendStatus(400)
+        })
+    } else {
+      ytdl.getBasicInfo(url)
+        .then(info => info.player_response)
+        .then(info => {
+          let duration = (info.lengthSeconds/60).toString()
+          duration = duration.substring(0, duration.indexOf('.'))+':'+Math.floor((info.lengthSeconds%60).toString())
+
 const max = info
     .videoDetails
     .thumbnail
     .thumbnails.reduce((prev, current) => ((prev.height > current.height) ? prev : current));
 
-res.json({
-    id: info.videoDetails.videoId,
+          videos.push({
+id: info.videoDetails.videoId,
     title: info.videoDetails.title,
     description: info.videoDetails.shortDescription,
     length: info.videoDetails.lengthSeconds,
@@ -19,14 +56,51 @@ res.json({
       date: info.videoDetails.publishDate,
    thumbnail: max,
    video:info.formats
-})
-} catch (exception) {
-        res.status(500).send(exception)
+          });
+          res.json(videos)
+        })
+        .catch(err => { 
+          res.statusMessage = "can't download video(s) data. probably wrong url : "+err
+          res.sendStatus(400)
+        })
     }
+  } catch(err) {
+    res.statusMessage = "can't download video(s) data. probably wrong url : "+err
+    res.sendStatus(400)
+  }
 })
 
-const PORT = process.env.PORT || 3000;
+app.get('/audio', async (req, res, next) => {
+  try {
+    var url = req.query.id;
+    res.header('Content-Disposition', `attachment; filename="audio.mp3"`);
+    ytdl(url, {
+      format: 'mp3',
+      filter: 'audioonly',
+    }).pipe(res);
+
+  } catch (err) {
+    res.statusMessage = err
+    res.sendStatus(400)
+  }
+});
+
+app.get('/video', async (req, res, next) => {
+  try {
+    var url = req.query.id;
+    res.header('Content-Disposition', `attachment; filename="audio.mp4"`);
+    ytdl(url, {
+      format: 'mp4',
+    }).pipe(res);
+
+  } catch (err) {
+    res.statusMessage = err
+    res.sendStatus(400)
+  }
+});
+
+
+let PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Listening at port :${PORT}`);
 });
