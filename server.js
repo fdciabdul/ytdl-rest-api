8e3056b45546ec3963996a1846237807616bc083
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2020-10-14 14:22:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ app.enable('trust proxy');
 const ytdl = require('ytdl-core');
 const ytpl = require('ytpl');
 const secure = require('ssl-express-www');
-var cookie = "VISITOR_INFO1_LIVE=tHFVSZc0Png; _gcl_au=1.1.1456374323.1601472163; PREF=f4=4000000&volume=100; SID=1wecW05dZ9cP6PsgaWbyui97xRbS_Q4e2o8pTW0xcojvMjBnwujtLRpKcE7yxD3rL1fP7A.; __Secure-3PSID=1wecW05dZ9cP6PsgaWbyui97xRbS_Q4e2o8pTW0xcojvMjBn-Vj0Rpv59T9iw3P9WxYndQ.; HSID=AKIvdEYGL-0dmPEcl; SSID=Atdi7GqzmotELNEG6; APISID=CRSiVL0dnwLT7U3w/A9notN-gQ85RNbRY6; SAPISID=T2d-Fsk2qACW-5Zc/AsY_l8-X9tkNM_PRT; __Secure-3PAPISID=T2d-Fsk2qACW-5Zc/AsY_l8-X9tkNM_PRT; CONSENT=YES+ID.en+201910; LOGIN_INFO=AFmmF2swRgIhAJjl4SVgjzp3VdEWhNoILsicJWp3rPj6Bbe1kWBCrDOdAiEAxBaj48_1bk5t5tNxc2dIIprC7HAwtBGIp5Bii2x1gYQ:QUQ3MjNmempLZGhxa2lQVklEQ1VYR3pyTjNOWmRBM1dNTTV2UmM4TFh5aDkxcFg1djhCMUVpWkdtX24yTGcxNW1Lam1uWUw3SnpMN2ZOWldrVVY3TTNyNHZwOERwN1I1THQteUNILUMtcm15SFdVUHpnMHlkM0xMNlVSV3N2Nl9iM3JFOVAtMXJOYlZwVkVOQTFYZXByaVJGekhDNlF4ZTk3VENlRkdsRUF1VEQ5OVV6SE9oU29jNjM5blg4WlNoSDBpay01YkRNcUts; SIDCC=AJi4QfGzCvWecIXBdQxe3R3FpEB3TMXX4heAj3utIRJEObPMfbPPnOPnz6lz31KXb3xB9ZNUHA; __Secure-3PSIDCC=AJi4QfElL3RNL-ZoWuN4UIYw8UGTqKG0ba4_NXZSNVvnV7cRmyEJl2kds4ZdKcGLLJmZ0kX0oQ; YSC=saZKly7xdkM";
+var cookie = "VISITOR_INFO1_LIVE=Dv_f-Nd_r4I;PREF=app=desktop&al=id&f4=4000000&volume=100;_gcl_au=1.1.2034278470.1602532760;SID=2QezCCxfUy7C4nRe_Y06pi9JNVISdrTKbDtHgb5Mb3VgBMhA3jDHnEDY_34xYojvbypLrg.;__Secure-3PSID=2QezCCxfUy7C4nRe_Y06pi9JNVISdrTKbDtHgb5Mb3VgBMhAjmS4dwt5hHtS28RtSz91nw.;HSID=AsXdh5tw6S_bQLh54;SSID=ARoxGdWp2T43ek6GZ;APISID=WgvKtXnk9yS7Im_A/AzuAK6xEq8FPf_CrJ;SAPISID=ZuZguUvG0wHIMoty/AHfigpkaCGy4O6814;__Secure-3PAPISID=ZuZguUvG0wHIMoty/AHfigpkaCGy4O6814;LOGIN_INFO=AFmmF2swRQIhAPZKOq9GWaJK6DdaODLCmJCzXoG1TyFBuLmeCW7EE8x4AiB6Axs_t3BBkfnS_e_qSYxRkkcRMX-lEm9izDHEDY4D3g:QUQ3MjNmeTY5NllCYVItWV9pMXBNc2pJaGFMMXFvVEItQmtDdU5jTnd4SGZQVmhsdXc5RDJIeUVRVXp5SFg4TW1EaFYyT0ExcjhSb1VQZThva0xLeTNmbC16c2lVSHNxZFRnZUE3NHpJUS1RVGZvNWRRV0pXRDdoNWhhRHdWb3NCQXMtUGhqaFlmTXN0NmhLZzBQQmpxNW5mMGdhWm9BSE0xc0lpSHFYQi1jZ3RrS2ZIdnhxQ1RJ;YSC=pGtOoS2kqpw;SIDCC=AJi4QfFY1eu4v_JhgeMdHpCOGHxfEcCqG0032_tl9ztBUuonjWmrvm-gjeTOssRBbMS7qaHMnQ;__Secure-3PSIDCC=AJi4QfFoR_FbCA1GDaMl0c_S-vj7IO0xEE9MSedOeWxnCsWZMR3msODYyWohL24GK0ecpHzK";
+
+
 app.use(morgan('common'));
 app.use(cors())
 app.use(secure)
@@ -15,29 +17,22 @@ app.use(express.json());
 app.get('/', async (req, res) => {
   let playlistregex = /\/playlist\?list=/;
   let videos = []
-  let url = req.query.url;
-  try {
-    if (playlistregex.test(url)) {
-      ytpl(url)
-        .then(info => info.items)
-        .then(info => {
-          let video
-          for (video of info) {
-            videos.push({
-              title: video.title,
-              id: video.id,
-              duration: video.duration,
-            });
-          }
-          res.json(videos)
-        })
-        .catch(err => { 
-          res.statusMessage = "can't download video(s) data. probably wrong url : "+err
-          res.sendStatus(400)
-        })
-    } else {
-      ytdl.getInfo(url , {requestOptions: { Cookie: cookie }})
-        .then(info => {
+  let url = req.query.url;  
+      ytdl.getInfo(url , 
+      { requestOptions: {
+      headers: {
+      cookie: cookie,
+      // Optional. If not given, ytdl-core will try to find it.
+      // You can find this by going to a video's watch page, viewing the source,
+      // and searching for "ID_TOKEN".
+      // 'x-youtube-identity-token': 1324,
+      'user-agent': 'Mozilla/5.0 (Linux; Android 8.1.0; vivo 1820) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.127 Mobile Safari/537.36',
+      'x-client-data':CIu2yQEIpLbJAQipncoBCJm1ygEIjrjKAQiZvMoBCKbIygEI58jKAQiCycoBCLTLygEIoc3KAQijzcoBCMrOygEItdXKAQjc1coBCLLXygEIwdfKAQiC2MoBCJ/YygEIodjKAQjE2MoBCOKYywEYsMjKAQ==',
+      'x-youtube-client-name': '1',
+      'x-youtube-client-version': '2.20180222'
+       
+    },
+      .then(info => {
           let duration = (info.lengthSeconds/60).toString()
           duration = duration.substring(0, duration.indexOf('.'))+':'+Math.floor((info.lengthSeconds%60).toString())
 
