326153250d2c57a5d853e4497b2c1d21d13f99d1
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2020-10-14 14:48:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -46,11 +46,6 @@ app.get('/', async (req, res) => {
       // You can find this by going to a video's watch page, viewing the source,
       // and searching for "ID_TOKEN".
       // 'x-youtube-identity-token': 1324,
-      user-agent: 'Mozilla/5.0 (Linux; Android 8.1.0; vivo 1820) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.127 Mobile Safari/537.36',
-      x-client-data: 'CIu2yQEIpLbJAQipncoBCJm1ygEIjrjKAQiZvMoBCKbIygEI58jKAQiCycoBCLTLygEIoc3KAQijzcoBCMrOygEItdXKAQjc1coBCLLXygEIwdfKAQiC2MoBCJ/YygEIodjKAQjE2MoBCOKYywEYsMjKAQ==',
-      x-youtube-client-name: '1',
-      x-youtube-client-version: '2.20180222'
-       
     },
   },
 })
