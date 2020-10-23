0d58a2bbc254fb665652b3954ec171a67d17ae45
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2020-10-24 01:03:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.enable('trust proxy');
 const ytdl = require('ytdl-core');
 const ytpl = require('ytpl');
 const secure = require('ssl-express-www');
-const cookie = "SID=2gezCNJJNrnxhriK7bvOqz8-ZuOE3GIl_lCv7kAYFoiMPHGcrtFQAY7PmOYMI64PTqUlUQ.;__Secure-3PSID=2gezCNJJNrnxhriK7bvOqz8-ZuOE3GIl_lCv7kAYFoiMPHGcZ0UT85gL3Uf5Jt25N-DJCQ.;HSID=A7EDzLn3kae2B1Njb;SSID=AheuwUjMojTWvA5GN;APISID=cgfXh13rQbb4zbLP/AlvlPJ2xBJBsykmS_;SAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;__Secure-3PAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;VISITOR_INFO1_LIVE=RgZLnZtCoPU;LOGIN_INFO=AFmmF2swRQIhAOXIXsKVou2azuz-kTsCKpbM9szRExAMUD-OwHYiuB6eAiAyPm4Ag3O9rbma7umBK-AG1zoGqyJinh4ia03csp5Nkw:QUQ3MjNmeXJ0UHFRS3dzaTNGRmlWR2FfMDRxa2NRYTFiN3lfTEdOVTc4QUlwbUI4S2dlVngxSG10N3ZqcHZwTHBKano5SkN2dDlPSkhRMUtReE42TkhYeUVWS3kyUE1jY2I1QzA1MDZBaktwd1llWU9lOWE4NWhoZV92aDkxeE9vMTNlcG1uMU9rYjhOaDZWdno2ZzN3TXl5TVNhSjNBRnJaMExrQXpoa2xzRVUteFNWZDI5S0Fn;YSC=8shK-NSCbaY;PREF=al=id&f4=4000000;SIDCC=AJi4QfFT7ZKhKrkdFC7K98GSFG7KOn9KmymtlbsuK65Aa4iMKd357hogNbLAhBhBRPKHSeKvmg;__Secure-3PSIDCC=AJi4QfFNBkYWxC9UvyQWm3vXy535JO_zHoEN7WuAl7lH-oVscImymWtGALUzw61-t-VkU2nR";
+//const cookie = "SID=2gezCNJJNrnxhriK7bvOqz8-ZuOE3GIl_lCv7kAYFoiMPHGcrtFQAY7PmOYMI64PTqUlUQ.;__Secure-3PSID=2gezCNJJNrnxhriK7bvOqz8-ZuOE3GIl_lCv7kAYFoiMPHGcZ0UT85gL3Uf5Jt25N-DJCQ.;HSID=A7EDzLn3kae2B1Njb;SSID=AheuwUjMojTWvA5GN;APISID=cgfXh13rQbb4zbLP/AlvlPJ2xBJBsykmS_;SAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;__Secure-3PAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;VISITOR_INFO1_LIVE=RgZLnZtCoPU;LOGIN_INFO=AFmmF2swRQIhAOXIXsKVou2azuz-kTsCKpbM9szRExAMUD-OwHYiuB6eAiAyPm4Ag3O9rbma7umBK-AG1zoGqyJinh4ia03csp5Nkw:QUQ3MjNmeXJ0UHFRS3dzaTNGRmlWR2FfMDRxa2NRYTFiN3lfTEdOVTc4QUlwbUI4S2dlVngxSG10N3ZqcHZwTHBKano5SkN2dDlPSkhRMUtReE42TkhYeUVWS3kyUE1jY2I1QzA1MDZBaktwd1llWU9lOWE4NWhoZV92aDkxeE9vMTNlcG1uMU9rYjhOaDZWdno2ZzN3TXl5TVNhSjNBRnJaMExrQXpoa2xzRVUteFNWZDI5S0Fn;YSC=8shK-NSCbaY;PREF=al=id&f4=4000000;SIDCC=AJi4QfFT7ZKhKrkdFC7K98GSFG7KOn9KmymtlbsuK65Aa4iMKd357hogNbLAhBhBRPKHSeKvmg;__Secure-3PSIDCC=AJi4QfFNBkYWxC9UvyQWm3vXy535JO_zHoEN7WuAl7lH-oVscImymWtGALUzw61-t-VkU2nR";
 
 
 
@@ -42,13 +42,13 @@ app.get('/', async (req, res) => {
       ytdl.getInfo(url , 
       { requestOptions: {
       headers: {
-      'Cookie': cookie,
-      'Referer':'https://www.youtube.com/InsideHeartz',
-      'X-client-data':'CIm2yQEIo7bJAQipncoBCL6nygEI4qjKAQ==',
+ //     'Cookie': cookie,
+//      'Referer':'https://www.youtube.com/InsideHeartz',
+//      'X-client-data':'CIm2yQEIo7bJAQipncoBCL6nygEI4qjKAQ==',
       'X-youtube-client-version':'2.20201013.02.02',
       'X-youtube-client-name': '1',
       'X-chrome-connected':'mode=0,enable_account_consistency=true',
-      'X-youtube-identity-token': 'QUFFLUhqbG1OVUx1UXlJazNsSjdLTE1WYzdyVFJPMEVyQXw=',
+   //   'X-youtube-identity-token': 'QUFFLUhqbG1OVUx1UXlJazNsSjdLTE1WYzdyVFJPMEVyQXw=',
       'X-youtube-device':'cbr=Chrome&cbrver=70.0.3538.110&ceng=WebKit&cengver=537.36&cos=X11&cosver=0'
     },
   },
@@ -95,13 +95,13 @@ app.get('/audio', async (req, res, next) => {
       filter: 'audioonly',
       requestOptions: {
 headers: {
-      'Cookie': cookie,
-      'Referer':'https://www.youtube.com/InsideHeartz',
-      'X-client-data':'CIm2yQEIo7bJAQipncoBCL6nygEI4qjKAQ==',
+//      'Cookie': cookie,
+//      'Referer':'https://www.youtube.com/InsideHeartz',
+//      'X-client-data':'CIm2yQEIo7bJAQipncoBCL6nygEI4qjKAQ==',
       'X-youtube-client-version':'2.20201013.02.02',
       'X-youtube-client-name': '1',
       'X-chrome-connected':'mode=0,enable_account_consistency=true',
-      'X-youtube-identity-token': 'QUFFLUhqbG1OVUx1UXlJazNsSjdLTE1WYzdyVFJPMEVyQXw=',
+//      'X-youtube-identity-token': 'QUFFLUhqbG1OVUx1UXlJazNsSjdLTE1WYzdyVFJPMEVyQXw=',
       'X-youtube-device':'cbr=Chrome&cbrver=70.0.3538.110&ceng=WebKit&cengver=537.36&cos=X11&cosver=0'
     },
 },
