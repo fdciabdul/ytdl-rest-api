bc112d92c6f991e1af05b1235fca548020129903
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2021-07-02 15:05:03 +0700

    Update lirik.js

--- lirik.js
+++ lirik.js
@@ -1,21 +1,60 @@
-const cheerio = require('cheerio')
 const axios = require('axios')
+const cheerio = require('cheerio')
 
-const lirikLagu = async (query) => {
-const res = await axios.get(`https://www.musixmatch.com/search/${query}`)
-const sup = cheerio.load(res.data)
-const hasil = []
-const b = sup('#site').find('div > div > div > div > ul > li:nth-child(1) > div > div > div')
-let link = `https://www.musixmatch.com` + sup(b).find('h2 > a').attr('href')
+// Add a connect listener
 
-const des = await axios.get(link)
-
-const soup = cheerio.load(des.data)
-
-const result = soup('#site').find('.mxm-lyrics__content > .lyrics__content__ok').text()
-
-hasil.push({ result})
-return hasil
+const test = function(x){
+	this.value = x
 }
 
-module.exports = { lirikLagu }
+async function lyrics(query){
+
+	const pageurl = `https:\/\/search.azlyrics.com/search.php?q=${query}`
+	
+	return axios.get(pageurl)
+		.then( (response)=>{
+			const html = response.data
+			const page = cheerio.load(html)
+			let result = page('.visitedlyr a')[0].attribs['href']
+
+			return axios.get(result)
+				.then((response)=>{
+					const html = response.data
+					const page = cheerio.load(html)
+
+					let lyrics = page('.ringtone')[0].next
+
+					let text = ''
+					while(true){
+						
+						if(lyrics.name == 'b' ){
+							// add title
+							text += lyrics.children[0].data
+						}
+
+
+						lyrics = lyrics.next
+						if (lyrics.type == 'tag' && lyrics.name=='div'){
+							break
+						}
+					}
+					
+					lyrics = text + lyrics.childNodes
+						.filter((tag)=> tag.type=='text').map((tag)=>tag.data).join()
+					test.value=lyrics
+					return(lyrics)
+
+				})
+				.catch((error)=>{
+					console.log("Error consulting", error)
+				})
+
+		}).catch((error)=>{
+			console.log("Error consulting ", error)
+		})
+	
+}
+
+module.exports = {
+	lyrics
+}
